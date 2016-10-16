3db4fa0af5cb88c3f6217d5d2b4e450fddaeb1fe
Dom <dom@idom.me>
2016-10-16 14:27:07 +0800

    Increase the version number.

--- src/compiler/program.ts
+++ src/compiler/program.ts
@@ -6,7 +6,7 @@ namespace ts {
     /** The version of the TypeScript compiler release */
 
     export const version = "2.0.5";
-    export const version_plus = "2.0.6";
+    export const version_plus = "2.0.10";
 
     const emptyArray: any[] = [];
 
