300dbc67a189624c68ca42713a95168e9908de86
Dom <dom@idom.me>
2016-10-20 18:36:49 +0800

    Allow passing null in the fileName parameter in the getEmitOutput function of service.

--- src/services/services.ts
+++ src/services/services.ts
@@ -7622,7 +7622,10 @@ namespace ts {
         function getEmitOutput(fileName: string, emitDeclarationsOnly?: boolean): EmitOutput {
             synchronizeHostData();
 
-            const sourceFile = getValidSourceFile(fileName);
+            let sourceFile:SourceFile;
+            if(fileName){
+                sourceFile = getValidSourceFile(fileName);
+            }
             const outputFiles: OutputFile[] = [];
 
             function writeFile(fileName: string, data: string, writeByteOrderMark: boolean) {
